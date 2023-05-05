6d6879a32e878254246dda0faee316de1a455d68
Chukwuemeka Abuba <abubachukwuemeka@outlook.com>
2023-05-05 04:34:02 +0100

    adding the basic portfolio setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Route, Routes } from "react-router-dom";
+import LeftStyledComponent from "./components/LeftStyledComponent";
+import NavBar from "./components/NavBar";
+import RightStyledComponent from "./components/RightStyledComponent";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Project from "./pages/Project";
 
+//root component
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar />
+    <LeftStyledComponent />
+    <RightStyledComponent />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/blog" element={<Blog/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/project" element={<Project/>} />
+      </Routes>
+    </>
   );
 }
 
